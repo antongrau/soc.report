0b8b9db18b14b450ec426edc3718cd079c0dad01
unknown <ronnietaarnborg@gmail.com>
2015-05-26 15:02:23 +0200

    nif

--- R/prepare_lime_data.R
+++ R/prepare_lime_data.R
@@ -1,5 +1,6 @@
 #' prepare.lime.data
 #' 
+#' Adds a number of attributes (header, sub.header, answer) to each variable in a data.frame. 
 #' 
 #' @param x a data.frame
 #' @return a data.frame
@@ -8,7 +9,10 @@
 prepare.lime.data <- function(x){
 
           if (!is.data.frame(x))
-                    stop("x is not a data.frame")
+            stop("x is not a data.frame")
+        
+          if (is.null(attributes(x)$variable.labels))
+            stop("x lacks variable.labels")
           
           var.lab <- attributes(x)$variable.labels
           headers <- answers <- vector("list", length=length(x))
@@ -17,8 +21,10 @@ prepare.lime.data <- function(x){
                     headers[i] <- gsub("(.*?)\\[.*?\\]", "\\1",var.lab[i])
                     answers[i] <- gsub(".*\\[(.*)\\].*", "\\1", var.lab[i])
                     
+                    headers[i] <- str_trim(headers[i])
+                    answers[i] <- str_trim(answers[i])
+                    
                     attributes(x[,i])$header <- headers[i]
-                    #attributes(df[,i])$answer <- answers[i]
                     
                     if (is.factor(x[,i]) && c("Not selected") %in% levels(x[,i])){
                               x[,i] <- revalue(x[,i], c("Yes" = answers[[i]]))
